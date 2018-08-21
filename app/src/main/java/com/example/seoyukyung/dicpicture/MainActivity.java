6283963e355ad15aa159c7b71cf7fd62e9ebbcf9
He Jeong <chojpsh1@gmail.com>
2018-08-21 11:53:40 +0900

    Update MainActivity.java

--- app/src/main/java/com/example/seoyukyung/dicpicture/MainActivity.java
+++ app/src/main/java/com/example/seoyukyung/dicpicture/MainActivity.java
@@ -43,7 +43,7 @@ import java.util.List;
 import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
 
-    private static final String CLOUD_VISION_API_KEY = "AIzaSyC9ucj6c1SJRSzRERLiheBrElqYfU76K_k";
+    private static final String CLOUD_VISION_API_KEY = "";
     public static final String FILE_NAME = "temp.jpg";
     private static final String ANDROID_CERT_HEADER = "X-Android-Cert";
     private static final String ANDROID_PACKAGE_HEADER = "X-Android-Package";
